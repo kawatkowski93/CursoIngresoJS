605e270af3c229c18973b04616f9ee797480089f
alumno <alumno>
2019-02-05 20:59:08 -0300

    sumas numero 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,14 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
-{
+{var numero1 =document.getElementById("numeroUno").value;
+    var numero2 =document.getElementById("numeroDos").value;
+    var resultado =parseInt(numero1)+parseInt(numero2);
+
+
+   
+
+    alert(resultado);
 
 }
 
