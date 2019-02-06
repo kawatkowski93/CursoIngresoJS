50bf26a8aad5a4df980914074750ac89fb601a61
alumno <alumno>
2019-02-05 21:29:15 -0300

    declarar las variables fuera de funciones

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,20 +2,37 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+var numero1=document.getElementById("numeroUno").value;
+var numero2=document.getElementById("numeroDos").value;
+var   resultado
+
+
 function sumar()
 {	
+
+var resultado =parseInt(numero1)+parseInt(numero2);
+
+aler(suma);
 	
 }
 
 function restar()
-{
-	
+
+{obtenerdatos()
+	var numero1=document.getElementById("numeroUno"),value;
+    var nuemro2=document.getElementById("nuemroDos"),value;
+    var resta =parseInt(numero1)-parseInt(numero2);
+
+    res
 }
 
+)
 function multiplicar()
 { 
 	
 }
+
+
 
 function dividir()
 {
