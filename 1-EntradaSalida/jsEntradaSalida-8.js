51b3eb5fb701689a58833d37435bbe2708aba12f
rodrigo <my@primerco.con>
2019-02-06 17:11:34 -0300

    intentando hacer el 8

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,7 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+    var numero1=document.getElementById().Value;
+    var numero2=document.getElementById().value;
+    var resultado=parseInt
 }
