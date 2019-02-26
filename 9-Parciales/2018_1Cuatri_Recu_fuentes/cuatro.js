53837a41677586d9471ef1975d9d11a5640add9c
rodrigo <rodrigo@kawa.com>
2019-02-26 20:26:49 -0300

    parci

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -5,8 +5,8 @@ function mostrar()
         var precioFinal;
         var precio;
         var tarjeta;
-        
-        
+
+
         libro=prompt("cantidad de libros ");
         libros=parseInt(libros);
 
@@ -15,13 +15,28 @@ function mostrar()
 
         tarjeta=confirm("paga con tarjeta ?");
 
-        
+
 
 
         if(libros>2){
-            
-        } 
-    
+
+        } // var  num 1 promt
+         var  num 2
+         var  num 3
+         var num 4
+         var mayor = numero1
+
+        if (num2>mayor)
+
+        mayor =numero2
+
+        if (numero 3  >mayor)
+
+        mayor= numero3
+
+        if numero4>mayor
+         mayor =numero 4
+
 
 
 
