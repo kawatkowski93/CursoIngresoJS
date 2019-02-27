4319f4626d507522e5707da7143b8fcbb6a9afdf
rodrigo <rodrigo@kawa.com>
2019-02-26 22:18:15 -0300

    parcial term

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,63 @@
+//cada habitación de un hotel tiene un precio, hay promociones según el tipo de pago,
+//si es con tarjeta visa un 10% , si es por paypal un 15%, por mercadoPago un 10%, efectivo
+//20 %, cualquier otro medio solo un 5%.
+//Si compraste el paquete “todoIncluido” y pagas con paypal se te suma un 10% al
+//descuento.
+//si pagas en efectivo, tenes varias opciones, el paquete “soloDesayunos” te suma un 10%
+//al descuento, si el paquete es “todoIncluido” te suma un 15% y para el resto de los
+//paquete no tiene descuento adicional
+//3- en tres.js
+//En un restaurante, un grupo de amigos quiere saber cuánto debe pagar cada uno//
+
+
+
 function mostrar()
+
 {
+    var precio
+    var descuento
+    var precioFinal
+    var medio
+    precio=prompt("ingrese el precio de la habitacion?");
+    medio=prompt("ingrese el medio de pago ");
+    switch(medio){
+        case "tarjetaVisa":
+        descuento=precio*0.1;
+        precioFinal=precio-descuento;
+
+
+        alert(precioFinal);
+        break;
+
+        case "paypal":
+                    switch(paquete){
+                          case "todoIncluido":
+
+                          break;
+                          default :
+
+                    }switch (tipo){
+
+                        case "Efectivo":
+                            switch(paquete)
+
+
+
+                    }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,6 +1,6 @@
 function mostrar()
 {
-        var libros ;
+        /*var libros ;
         var descuento;
         var precioFinal;
         var precio;
@@ -18,36 +18,60 @@ function mostrar()
 
 
 
-        if(libros>2){
+        if(libros>2){*/
 
-        } // var  num 1 promt
-         var  num 2
-         var  num 3
-         var num 4
-         var mayor = numero1
+        /*}   comparar 4 numeros  ejercio 1 */
+         /*var  num1= prompt("primer numero");
+         var  num2 =prompt("segundo numero");
+         var  num3=prompt("tercernumero");
+          var num4=prompt("tercer numero");
+         var mayor = num1
 
-        if (num2>mayor)
+        if (num2>mayor){
 
-        mayor =numero2
+        mayor =num2;
 
-        if (numero 3  >mayor)
+        }if (num3>mayor){
 
-        mayor= numero3
+        mayor= num3;
 
-        if numero4>mayor
-         mayor =numero 4
+        }if (num4>mayor){
+         mayor =num4;
+        }alert(mayor+ "el mayor es");
+              } llave final */
 
-
-
-
-
-
-
-
-
-
+          var contador =0;
+           var mayor
+           var orden
+           var num
 
 
+            while(contador<4){
+              contador++;
+              do{
+              num=prompt("ingrese los  numeros restantes ");
+              num=parseInt(num);
+
+              }while(isNaN(num));// aca valido  si es un  numero  ingresa el dato solo una vez una vez que paso ya esta  valido  i sigue con  las condiciones .
+
+              if(contador==1){ // esta es la  primera interaccion
+                orden=contador// va a ser  mi primer mayor  el primer numero
+                mayor=num;
+
+              }else{
+                if(num>mayor){
+                  mayor=num// aca comparo de esa  primera  interaccion si  el numero es mayor
+
+                  orden=contador
+                }
 
 
-}
+
+          }
+
+
+
+
+
+
+  }
